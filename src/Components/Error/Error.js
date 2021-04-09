d70cc09a8df3d1279e7eafde9c31db4d0c9cc1cc
MuckT <mcgtom10@gmail.com>
2021-04-08 22:07:44 -0700

    feat: add error handling

---
+++ src/Components/Error/Error.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+import './Error.css';
+
+class Error extends React.Component {
+  refreshPage = () => { 
+    window.location.reload(); 
+  }
+
+  render() {
+    return (
+      <Jumbotron fluid>
+        <h1>Opps! Something went wrong.</h1>
+        <p>{this.props.errors[0].status} - {this.props.errors[0].errorMsg}</p>
+        <Button onClick={this.refreshPage} variant="primary" type="submit">
+          Search again
+        </Button>
+      </Jumbotron>
+    )
+  }
+}
+
+export default Error;
