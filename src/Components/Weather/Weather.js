0c8cee2b63f5d2197c4737aea0e58de92c317940
MuckT <mcgtom10@gmail.com>
2021-04-13 01:39:20 -0700

    feat: weather component

---
+++ src/Components/Weather/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Jumbotron, ListGroup } from 'react-bootstrap';
+
+class Weather extends React.Component {
+  render() {
+    let weather = [];
+    this.props.weather.forEach((item, index) => {
+      weather.push(
+        <ListGroup.Item key={index}> {item.date} - {item.description} </ListGroup.Item>
+      );
+    });
+    return <Jumbotron><ListGroup variant="flush">{weather}</ListGroup></Jumbotron>;
+  }
+}
+
+export default Weather;
