6071dd80e3f00eb7b7d5ec6406203c208eb4d493
MuckT <mcgtom10@gmail.com>
2021-04-14 20:37:49 -0700

    chore: use map for weather component

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -5,12 +5,7 @@ import { Jumbotron, ListGroup } from 'react-bootstrap';
 
 class Weather extends React.Component {
   render() {
-    let weather = [];
-    this.props.weather.forEach((item, index) => {
-      weather.push(
-        <ListGroup.Item key={index}> {item.date} - {item.description} </ListGroup.Item>
-      );
-    });
+    let weather = this.props.weather.map((item, index) => <ListGroup.Item key={index}> {item.date} - {item.description} </ListGroup.Item>);
     return <Jumbotron><ListGroup variant="flush">{weather}</ListGroup></Jumbotron>;
   }
 }
